b69805d356af40cd8433d971734910f7dc9b89a6
ClicShopping <Yae8mr2o*>
2020-02-10 11:19:50 -0500

    small update

--- sources/template/Default/modules/modules_products_info/pi_products_info_stock.php
+++ sources/template/Default/modules/modules_products_info/pi_products_info_stock.php
@@ -102,11 +102,11 @@
       $CLICSHOPPING_Db->save('configuration', [
           'configuration_title' => 'A quel endroit souhaitez-vous afficher l\'état du stock ?',
           'configuration_key' => 'MODULE_PRODUCTS_INFO_STOCK_POSITION',
-          'configuration_value' => 'none',
+          'configuration_value' => 'float-md-none',
           'configuration_description' => 'Affiche l\'état du stock du produit à gauche ou à droite<br><br><i>(Valeur Left = Gauche <br>Valeur Right = Droite <br>Valeur None = Aucun)</i>',
           'configuration_group_id' => '6',
           'sort_order' => '2',
-          'configuration_value' => 'none',
+          'set_function' => 'clic_cfg_set_boolean_value(array(\'float-md-right\', \'float-md-left\' \'float-md-none\') ',
           'date_added' => 'now()'
         ]
       );
